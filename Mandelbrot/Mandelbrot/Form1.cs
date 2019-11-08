389bdeefd2128d8df192e0bd84ffc1751720ca1b
Jhonathan Ailon <xitumuldemetrio@hotmail.com>
2019-11-08 13:33:16 -0600

    jenkins

--- Mandelbrot/Mandelbrot/Form1.cs
+++ Mandelbrot/Mandelbrot/Form1.cs
@@ -26,7 +26,7 @@ namespace Mandelbrot
         public void MandelbrotSet()
         {
 
-            //PRUEBA mandelbrot
+            //PRUEBA mandelbrot v1
             int width = pictureBox1.Width;
             int heigh = pictureBox1.Height;
             Bitmap bmp = new Bitmap(width, heigh);
